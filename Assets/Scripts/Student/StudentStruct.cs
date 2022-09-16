23a30bd8690c28dec720303f7c4f382eb9e7c7a1
Tormod Liseth <tormod@sifterstudios.no>
2022-09-16 20:47:39 +0200

    Working on UI, unity crashes all the time..

---
+++ Assets/Scripts/Student/StudentStruct.cs
@@ -0,0 +1,11 @@
+using Sifter.Degree;
+
+public struct StudentStruct
+{
+    public string FirstName;
+    public string LastName;
+    public string Email;
+    public int PhoneNumber;
+    public bool AccessibilityRequirement;
+    public DegreeEnum Degree;
+}
